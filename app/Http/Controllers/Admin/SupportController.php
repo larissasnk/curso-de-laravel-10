bdcbb17f733eebe918a7d10392ffea2e69237685
larissank <larissasouza201565@gmail.com>
2024-11-02 13:45:08 -0300

    update support

--- app/Http/Controllers/Admin/SupportController.php
+++ app/Http/Controllers/Admin/SupportController.php
@@ -15,6 +15,17 @@ class SupportController extends Controller
         return view('admin/supports/index', compact('supports'));
     }
 
+    public function show(string|int $id)
+    {
+        //Support::find($id)
+        //Support::where('id', $id)->first();
+        //Support::where('id', '=', $id)->first();
+        if (!$support = Support::find($id)) {
+            return back();
+        }
+        return view('admin/supports/show', compact('support'));
+    }
+
     public function create()
     {
         return view('admin/supports/create');
@@ -28,6 +39,32 @@ class SupportController extends Controller
         $support->create($data);
 
         return redirect()->route('supports.index');
+    }
 
+    public function edit(Support $support, string|int $id)
+    {
+        if (!$support = $support->where('id', $id)->first()) {
+            return back();
+        }
+
+        return view('admin/supports.edit', compact('support'));
+    }
+
+    public function update(Request $request, Support $support, string $id)
+    {
+        if (!$support = $support->find($id)) {
+            return back();
+        }
+
+        // $support->subject = $request->subject;
+        // $support->body = $request->body;
+        // $support->save();
+
+        $support->update($request->only([
+            'subject',
+            'body'
+        ]));
+
+        return redirect()->route('supports.index');
     }
 }
