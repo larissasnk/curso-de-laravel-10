f24206b6d73cdc168053ca45904c9d8fab4ed644
larissank <larissasouza201565@gmail.com>
2024-11-02 22:48:54 -0300

    att refatoração

--- app/Http/Controllers/Admin/SupportController.php
+++ app/Http/Controllers/Admin/SupportController.php
@@ -2,26 +2,40 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\DTO\Supports\CreateSupportDTO;
+use App\DTO\Supports\UpdateSupportDTO;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreUpdateSupport;
 use App\Models\Support;
+use App\Services\SupportService;
 use Illuminate\Http\Request;
 use PhpParser\Node\Expr\FuncCall;
 
 class SupportController extends Controller
 {
-    public function index(Support $support)
-    {
-        $supports = $support->all();
+    public function __construct(
+        protected SupportService $service
+    ) {}
 
-        return view('admin/supports/index', compact('supports'));
+    public function index(Request $request)
+    {
+        $supports = $this->service->paginate(
+            page: $request->get('page', 1),
+            totalPerPage: $request->get('per_page', 1),
+            filter: $request->filter,
+        );
+
+        $filters = ['filter' => $request->get('filter', '')];
+
+        return view('admin/supports/index', compact('supports','filters'));
     }
 
-    public function show(string|int $id)
+    public function show(string $id)
     {
         //Support::find($id)
         //Support::where('id', $id)->first();
         //Support::where('id', '=', $id)->first();
-        if (!$support = Support::find($id)) {
+        if (!$support = $this->service->findOne($id)) {
             return back();
         }
         return view('admin/supports/show', compact('support'));
@@ -32,49 +46,41 @@ class SupportController extends Controller
         return view('admin/supports/create');
     }
 
-    public function store(Request $request, Support $support)
+    public function store(StoreUpdateSupport $request, Support $support)
     {
-        $data = $request->all();
-        $data['status'] = 'a';
+        $this->service->new(
+            CreateSupportDTO::makeFromRequest($request)
 
-        $support->create($data);
+        );
 
         return redirect()->route('supports.index');
     }
 
-    public function edit(Support $support, string|int $id)
+    public function edit(string $id)
     {
-        if (!$support = $support->where('id', $id)->first()) {
+        // if (!$support = $support->where('id', $id)->first()) {
+        if (!$support = $this->service->findOne($id)) {
             return back();
         }
 
         return view('admin/supports.edit', compact('support'));
     }
 
-    public function update(Request $request, Support $support, string $id)
+    public function update(StoreUpdateSupport $request, Support $support, string $id)
     {
-        if (!$support = $support->find($id)) {
+        $support = $this->service->update(
+            UpdateSupportDTO::makeFromRequest($request)
+        );
+        if (!$support) {
             return back();
         }
-
-        // $support->subject = $request->subject;
-        // $support->body = $request->body;
-        // $support->save();
-
-        $support->update($request->only([
-            'subject',
-            'body'
-        ]));
-
+        
         return redirect()->route('supports.index');
     }
 
-    public function destroy(string|int $id)
+    public function destroy(string $id)
     {
-        if (!$support = Support::find($id)) {
-            return back();
-        }
-        $support->delete();
+        $this->service->delete($id);
 
         return redirect()->route('supports.index');
     }
--- resources/views/admin/supports/edit.blade.php
+++ resources/views/admin/supports/edit.blade.php
@@ -1,10 +1,10 @@
-<h1>Dúvida{{ $support->id }}</h1>
+<h1>Dúvida {{ $support->id }}</h1>
+
+<x-alert/>
 
 <form action="{{ route('supports.update', $support->id) }}" method="POST">
-    {{-- <input type="hidden" value="{{csrf_token()}}" name="_token"> --}}
-    @csrf()
     @method('PUT')
-    <input type="text" placeholder="Assunto" name="subject" value="{{ $support->subject }}">
-    <textarea name="body" id="" cols="30" rows="5" placeholder="Descrição">{{ $support->body }}</textarea>
-    <button type="submit">Enviar</button>
+    @include('admin.supports.partials.form', [
+        'supports' => $support
+    ])
 </form>
